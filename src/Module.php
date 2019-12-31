020f7bf516b76519ae2517a52009f7b5fd304783
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:09:33 -0600

    Rewriting as Laminas Project package

--- src/Module.php
+++ src/Module.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-i18n for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-i18n for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\I18n;
+namespace Laminas\I18n;
 
 class Module
 {
     /**
-     * Return zend-i18n configuration for zend-mvc application.
+     * Return laminas-i18n configuration for laminas-mvc application.
      *
      * @return array
      */
