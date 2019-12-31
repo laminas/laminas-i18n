1fc017fe18d9b572514d5256e5b92d024dc60d80
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:09:13 -0600

    Rewriting as Laminas Project package

--- src/Filter/Alnum.php
+++ src/Filter/Alnum.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-i18n for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\I18n\Filter;
+namespace Laminas\I18n\Filter;
 
 use Locale;
 use Traversable;
@@ -64,7 +63,7 @@ class Alnum extends AbstractLocale
     }
 
     /**
-     * Defined by Zend\Filter\FilterInterface
+     * Defined by Laminas\Filter\FilterInterface
      *
      * Returns $value as string with all non-alphanumeric characters removed
      *
