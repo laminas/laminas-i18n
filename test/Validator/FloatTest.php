93ab6de5a41e3845aff9297730076c63fc3a7dfc
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:09:01 -0600

    Rewriting as Laminas Project package

--- test/Validator/FloatTest.php
+++ test/Validator/FloatTest.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-i18n for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\I18n\Validator;
+namespace LaminasTest\I18n\Validator;
 
-use Zend\I18n\Validator\Float as FloatValidator;
+use Laminas\I18n\Validator\Float as FloatValidator;
 use Locale;
 
 /**
- * @group      Zend_Validator
+ * @group      Laminas_Validator
  */
 class FloatTest extends \PHPUnit_Framework_TestCase
 {
