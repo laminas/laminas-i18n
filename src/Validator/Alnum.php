ec7c331588176351bf919e5ed767f67363b0f484
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:06:55 -0600

    Rewriting as Laminas Project package

--- src/Validator/Alnum.php
+++ src/Validator/Alnum.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_I18n
+ * @see       https://github.com/laminas/laminas-i18n for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\I18n\Validator;
+namespace Laminas\I18n\Validator;
 
-use Zend\I18n\Filter\Alnum as AlnumFilter;
-use Zend\Validator\AbstractValidator;
+use Laminas\I18n\Filter\Alnum as AlnumFilter;
+use Laminas\Validator\AbstractValidator;
 
 /**
- * @category   Zend
- * @package    Zend_I18n
+ * @category   Laminas
+ * @package    Laminas_I18n
  * @subpackage Validator
  */
 class Alnum extends AbstractValidator
