f8deca04ae73957d69f010c5142728a968d3f4af
George Steel <george@net-glue.co.uk>
2024-07-01 22:55:00 +0100

    `Laminas\Translator`
    Requires `laminas/laminas-translator` and extends the shipped interface via `Laminas\I18n\Translator\TranslatorInterface`
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Translator/TranslatorInterface.php
+++ src/Translator/TranslatorInterface.php
@@ -2,36 +2,11 @@
 
 namespace Laminas\I18n\Translator;
 
+use Laminas\Translator\TranslatorInterface as Translator;
+
 /**
  * Translator interface.
  */
-interface TranslatorInterface
+interface TranslatorInterface extends Translator
 {
-    /**
-     * Translate a message.
-     *
-     * @param  string $message
-     * @param  string $textDomain
-     * @param  string $locale
-     * @return string
-     */
-    public function translate($message, $textDomain = 'default', $locale = null);
-
-    /**
-     * Translate a plural message.
-     *
-     * @param  string      $singular
-     * @param  string      $plural
-     * @param  int         $number
-     * @param  string      $textDomain
-     * @param  string|null $locale
-     * @return string
-     */
-    public function translatePlural(
-        $singular,
-        $plural,
-        $number,
-        $textDomain = 'default',
-        $locale = null
-    );
 }
