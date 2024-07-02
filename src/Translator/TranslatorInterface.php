5bd49f80cafe8b1439c26456b617542879fa26b7
George Steel <george@net-glue.co.uk>
2024-07-02 08:31:36 +0100

    Deprecate `TranslatorInterface` in favour of the interface shipped by `laminas/laminas-translator`
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Translator/TranslatorInterface.php
+++ src/Translator/TranslatorInterface.php
@@ -6,6 +6,9 @@ use Laminas\Translator\TranslatorInterface as Translator;
 
 /**
  * Translator interface.
+ *
+ * @deprecated Since 2.27.0 The translator interface is now in a separate package `laminas/laminas-translator` and this
+ *             interface should be replaced by `Laminas\Translator\TranslatorInterface`
  */
 interface TranslatorInterface extends Translator
 {
