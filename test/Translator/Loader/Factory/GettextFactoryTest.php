dcec45b3266a232406ffc452087c4d0a68303133
George Steel <george@net-glue.co.uk>
2025-12-31 16:08:05 +0000

    Add happy path test
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/Translator/Loader/Factory/GettextFactoryTest.php
+++ test/Translator/Loader/Factory/GettextFactoryTest.php
@@ -6,6 +6,7 @@ namespace LaminasTest\i18n\Translator\Loader\Factory;
 
 use Laminas\I18n\Exception\RuntimeException;
 use Laminas\I18n\Translator\Loader\Factory\GettextFactory;
+use Laminas\I18n\Translator\Loader\Gettext;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 
@@ -29,5 +30,16 @@ final class GettextFactoryTest extends TestCase
         $this->expectExceptionMessage('Gettext include path option must be boolean');
 
         $factory->__invoke($container, 'foo');
+    }
+
+    public function testCanProduceInstanceWithNoConfig(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())->method('has')->with('config')->willReturn(false);
+        $factory = new GettextFactory();
+        self::assertInstanceOf(
+            Gettext::class,
+            $factory->__invoke($container, 'foo'),
+        );
     }
 }
